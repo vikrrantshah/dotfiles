fad60194e3533d5f4b94e7cd2ae915edc37cd7dc
Vikrant Shah <60457380+vikrrantshah@users.noreply.github.com>
2025-07-17 09:57:11 +0200

    Add arbio sessions

--- tmux/tmux-sessionizer/projects/arbio-nexus.sh
+++ tmux/tmux-sessionizer/projects/arbio-nexus.sh
@@ -2,30 +2,30 @@
 
 open -a Docker
 
-SESSION="sedomo"
+SESSION="arbio-nexus"
 SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
 
-CD_FRONTEND='cd housemeister-app'
-CD_BACKEND='cd housemeister-backend'
+CD_FRONTEND='cd nexus'
+CD_BACKEND='cd backend'
 
 if [ "$SESSIONEXISTS" = "" ]; then
   tmux new-session -d -s $SESSION
 
   # Frontend Editor Window
-  tmux rename-window -t 0 'Frontend'
-  tmux send-keys -t 'Frontend' "$CD_FRONTEND" C-m 'nvim' C-m
+  tmux rename-window -t 0 'Nexus'
+  tmux send-keys -t 'Nexus' "$CD_FRONTEND" C-m 'nvim' C-m
 
   # Backend Editor Window
   tmux new-window -t $SESSION:1 -n 'Backend'
   tmux send-keys -t 'Backend' "$CD_BACKEND" C-m 'nvim' C-m
 
   # Frontend Server Window
-  tmux new-window -t $SESSION:2 -n 'Frontend-NIX'
-  tmux send-keys -t 'Frontend-NIX' "$CD_FRONTEND" C-m 'nix develop' C-m 'yarn dev' C-m
+  tmux new-window -t $SESSION:2 -n 'Neuxs Server'
+  tmux send-keys -t 'Neuxs Server' "$CD_FRONTEND" C-m 'npm run dev' C-m
 
   # Backend Server Window
-  tmux new-window -t $SESSION:3 -n 'Backend-NIX'
-  tmux send-keys -t 'Backend-NIX' "$CD_BACKEND" C-m 'nix develop' C-m 'docker compose up -d' C-m 'symfony serve' C-m
+  tmux new-window -t $SESSION:3 -n 'Backend Server'
+  tmux send-keys -t 'Backend Server' "$CD_BACKEND" C-m 'docker compose up -d' C-m 'docker compose stop app' C-m 'npm run start:dev' C-m
 
   # Shell Panes for both Frontend and Backend
   tmux new-window -t $SESSION:4 -n 'Shells'
